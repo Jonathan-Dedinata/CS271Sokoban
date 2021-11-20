fe760ac3af61212446d9148161d60ff30a128a5d
Zhecheng Huang <hw87147@gmail.com>
2021-11-19 17:20:49 -0800

    v0.000003 update player move method, now it can on target

--- game.py
+++ game.py
@@ -5,13 +5,13 @@ import tkinter as tk
 
 def next_position(direction, _x, _y):
     if direction == 1:
-        return _x-1, _y
+        return _x - 1, _y
     elif direction == 2:
-        return _x+1, _y
+        return _x + 1, _y
     elif direction == 3:
-        return _x , _y -1
+        return _x, _y - 1
     elif direction == 4:
-        return _x , _y +1
+        return _x, _y + 1
     else:
         raise AttributeError("wrong direction")
 
@@ -30,35 +30,37 @@ class game:
         self.player_y = _player_y
         self.number_of_box = number
         self.board = data
-        self.number_of_box_on_target = 0 #fix latter
+        self.number_of_box_on_target = 0  # fix latter
 
         print("unfinished")
 
     def hit_wall_check(self, direction):
         return self.board[next_position(direction, self.player_x, self.player_y)].is_wall()
 
+    def hit_block_check(self, direction):
+        return self.board[next_position(direction, self.player_x, self.player_y)].tag == 0
+
     def hit_box_check(self, direction):
         return self.board[next_position(direction, self.player_x, self.player_y)].is_box()
+
+    def hit_target_check(self, direction):
+        return self.board[next_position(direction, self.player_x, self.player_y)].is_target()
 
     def make_push(self, direction):
         exit(1)
 
     def make_move(self, direction):
         if direction == 1:
-            self.board[(self.player_x, self.player_y)] = block.block(self.player_x, self.player_y)
-            self.board[(self.player_x -1, self.player_y)] = block.player(self.player_x -1, self.player_y)
+
             self.player_x -= 1
         elif direction == 2:
-            self.board[(self.player_x, self.player_y)] = block.block(self.player_x, self.player_y)
-            self.board[(self.player_x+1, self.player_y )] = block.player(self.player_x+1, self.player_y + 1)
+
             self.player_x += 1
         elif direction == 3:
-            self.board[(self.player_x, self.player_y)] = block.block(self.player_x, self.player_y)
-            self.board[(self.player_x , self.player_y-1)] = block.player(self.player_x  , self.player_y-1)
+
             self.player_y -= 1
         elif direction == 4:
-            self.board[(self.player_x, self.player_y)] = block.block(self.player_x, self.player_y)
-            self.board[(self.player_x , self.player_y+1)] = block.player(self.player_x, self.player_y+1)
+
             self.player_y += 1
 
     def step(self, direction):
@@ -69,20 +71,20 @@ class game:
             exit(1)
         if direction == 1:
             if not self.hit_wall_check(direction):
-                self.make_move(1)
+                if self.hit_block_check(direction) or self.hit_target_check(direction):
+                    self.make_move(1)
         if direction == 2:
             if not self.hit_wall_check(direction):
-                self.make_move(2)
+                if self.hit_block_check(direction) or self.hit_target_check(direction):
+                    self.make_move(2)
         if direction == 3:
             if not self.hit_wall_check(direction):
-                self.make_move(3)
+                if self.hit_block_check(direction) or self.hit_target_check(direction):
+                    self.make_move(3)
         if direction == 4:
             if not self.hit_wall_check(direction):
-                self.make_move(4)
-
-
-
-
+                if self.hit_block_check(direction) or self.hit_target_check(direction):
+                    self.make_move(4)
 
 
 def read_input(file_name):
@@ -116,23 +118,22 @@ def read_input(file_name):
         grids[x, y] = block.target(x, y)
     x = data_flow.pop(0)
     y = data_flow.pop(0)
-    grids[x, y] = block.player(x, y)
+    # grids[x, y] = block.player(x, y)
 
-    for i in range(1,vertical_length+1):
-        for j in range(1,horizontal_length+1):
-            if (i,j) not in grids:
-                grids[i,j]  = block.block(i,j)
-
+    for i in range(1, vertical_length + 1):
+        for j in range(1, horizontal_length + 1):
+            if (i, j) not in grids:
+                grids[i, j] = block.block(i, j)
 
     return data_flow, grids, vertical_length, horizontal_length, number_of_box, x, y
 
 
-def draw(canvas, grids_data):
+def draw(canvas, grids_data, x, y):
     for key, val in grids_data.items():
         if grids_data[key].is_wall():
             canvas.itemconfig(grids[key[0] - 1][key[1] - 1], fill='brown')
-        elif grids_data[key].is_player():
-            canvas.itemconfig(grids[key[0] - 1][key[1] - 1], fill='blue')
+        # elif grids_data[key].is_player():
+        # canvas.itemconfig(grids[key[0] - 1][key[1] - 1], fill='blue')
         elif grids_data[key].is_box():
             canvas.itemconfig(grids[key[0] - 1][key[1] - 1], fill='yellow')
         elif grids_data[key].is_target():
@@ -141,6 +142,10 @@ def draw(canvas, grids_data):
             canvas.itemconfig(grids[key[0] - 1][key[1] - 1], fill='black')
         else:
             canvas.itemconfig(grids[key[0] - 1][key[1] - 1], fill='white')
+
+    print(canvas.bbox(grids[x - 1][y - 1]))
+    canvas.delete('player')
+    canvas.create_oval(canvas.bbox(grids[x - 1][y - 1]), fill='grey', tag='player')
     return canvas
 
 
@@ -173,7 +178,7 @@ if __name__ == "__main__":
 
     # draw
 
-    canvas = draw(canvas, grids_data)
+    canvas = draw(canvas, grids_data, x, y)
 
     canvas.pack()
     g = game(x, y, n, grids_data)
@@ -181,31 +186,31 @@ if __name__ == "__main__":
 
     def up():
         g.step(1)
-        draw(canvas,g.board)
+        draw(canvas, g.board, g.player_x, g.player_y)
         print("up")
         return canvas
 
 
     def down():
         g.step(2)
-        draw(canvas, g.board)
+        draw(canvas, g.board, g.player_x, g.player_y)
         print("down")
         return canvas
 
 
-
     def left():
         g.step(3)
-        draw(canvas, g.board)
+        draw(canvas, g.board, g.player_x, g.player_y)
         print("left")
         return canvas
 
 
     def right():
         g.step(4)
-        draw(canvas, g.board)
+        draw(canvas, g.board, g.player_x, g.player_y)
         print("right")
         return canvas
+
 
     b_up = tk.Button(window, text='UP', command=up).place(x=100, y=700)
     b_down = tk.Button(window, text='DOWN', command=down).place(x=200, y=700)
